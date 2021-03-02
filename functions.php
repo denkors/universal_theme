b10b30da1fd04827140126f0df17a06859d94b1d
denkors <denoff.01@gmail.com>
2021-03-02 23:31:50 +0300

    Added universal_theme

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+//добавление расширенных возможностей
+if ( ! function_exists( 'universal_theme_setup' ) ) :
+  
+  function universal_theme_setup(){
+    //добавление тега title 
+    add_theme_support( 'title-tag' );
+    //логотип 
+    add_theme_support( 'custom-logo', [
+	'width'       => 163,
+	'flex-height' => true,
+	'header-text' => 'Universal',
+	'unlink-homepage-logo' => false, // WP 5.5
+  ] );
+  //регистрация меню
+	  register_nav_menus( [
+		  'header_menu' => 'Меню в шапке',
+		  'footer_menu' => 'Меню в подвале'
+    ]);
+  }
+endif;
+add_action( 'after_setup_theme', 'universal_theme_setup' );
+//подключение стилей и скриптов
+function enqueue_universal_style() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'universal_theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+//свой контент
+?>
+<h1>Привет,мир!</h1>
+<h2>https://github.com/denkors/my_first_rep</h2>
+<php?
+get_footer();
+?>
