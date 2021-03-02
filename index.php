68ed58e5abddacb81b2c8cc4bc7a6e3f04c20940
denkors <denoff.01@gmail.com>
2021-03-03 00:09:22 +0300

    added index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ get_header();
 //свой контент
 ?>
 <h1>Привет,мир!</h1>
-<h2>https://github.com/denkors/my_first_rep</h2>
+<h2>https://github.com/denkors/universal_theme</h2>
 <php?
 get_footer();
 ?>
