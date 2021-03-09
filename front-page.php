e54a9dfc6c8574298c351f8cff6856ba5c4fda12
denkors <denoff.01@gmail.com>
2021-03-10 00:43:11 +0300

    fics

--- front-page.php
+++ front-page.php
@@ -29,7 +29,7 @@
         </a>
         <div class="post-text">
           <?php the_category() ?>
-          <h2 class="post-title"><?php the_title() ?></h2>
+          <h2 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h2>
           <a href="<?php echo get_the_permalink() ?>" class="more">Читать далее</a>      
         </div>
                     <?php 
@@ -55,6 +55,7 @@
           $myposts = get_posts([ 
             'numberposts' => 5,
             'offset' => 1,
+            'category_name' => 'java-script,css,html,web-design',
             
           ]);
             //проверяем есть ли посты
@@ -67,7 +68,7 @@
           <li class="post">
             <?php the_category() ?>
             <a class="post-permalink" href="<?php echo get_the_permalink() ?>">
-              <h4 class="post-title"><?php the_title() ?></h4>
+              <h4 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...') ?></h4>
             </a>
           </li>
           <?php 
@@ -85,5 +86,46 @@
       </div>
       </div>
 
-    </div>
+    
   </div>
+</main>
+<!-- /container -->
+<div class="container">
+        <ul class="article-list">
+            <?php
+            //обьявляем глобальную переменную
+            global $post;
+
+            $myposts = get_posts([ 
+              'numberposts' => 4,
+              'category_name' => 'articles',
+            
+              
+            ]);
+              //проверяем есть ли посты
+            if( $myposts ){
+              //если есть то запускаем цикл
+              foreach( $myposts as $post ){
+                setup_postdata( $post );
+                ?>
+              <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+            <li class="article-item">
+                  <a class="article-permalink" href="<?php echo get_the_permalink() ?>">
+                    <h4 class="article-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h4>
+                  </a>
+                  <img width="65" height="65" src="<?php echo get_the_post_thumbnail_url( null, 'homepage-thumb')?> " alt="" class="">
+            </li>
+              <?php 
+              }
+            } else {
+              // Постов не найдено
+              ?>
+              <p>Записей нет</p> 
+              <?php
+            }
+
+            wp_reset_postdata(); // Сбрасываем $post
+            ?>
+        </ul>
+</div>
+<!-- /container -->
